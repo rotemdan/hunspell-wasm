33af51bb80ee90a02b92fe300c45e017b745dcf7
Rotem Dan <rotemdan@gmail.com>
2024-10-30 18:52:53 +0200

    Remove `dist` from repository

--- dist/TypedArray.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=TypedArray.js.map
--- dist/Utilities.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function concatUint8Arrays(arrays: Uint8Array[]): Uint8Array;
-export declare function getRandomId(): string;
